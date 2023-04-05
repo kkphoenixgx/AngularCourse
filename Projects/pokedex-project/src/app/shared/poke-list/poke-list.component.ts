b22173abc38c1fb63434d8b25c37556ea3a88b1f
Kauã Alves Santos <kauaAlvesWorkplace@gmail.com>
2023-04-05 19:59:13 -0300

    App finished

--- Projects/pokedex-project/src/app/shared/poke-list/poke-list.component.ts
+++ Projects/pokedex-project/src/app/shared/poke-list/poke-list.component.ts
@@ -7,21 +7,34 @@ import { PokeApiService } from 'src/app/service/poke-api.service';
   styleUrls: ['./poke-list.component.scss']
 })
 export class PokeListComponent implements OnInit {
-  
-  public getAllPokes :any
-  
+
+  public allPokes: any
+  public getAllPokes: any
+
+  public apiError :boolean = false
+
   constructor(
-    private pokeApiService :PokeApiService
-  ){}
+    private pokeApiService: PokeApiService
+  ) { }
 
   ngOnInit(): void {
     this.pokeApiService.listPokes.subscribe(
       res => {
-        this.getAllPokes = res.results;
-        console.log(this.getAllPokes);
+        this.allPokes = res.results;
+        this.getAllPokes = this.allPokes;
+      },
+      error => {
+        this.apiError = true
       }
     );
-    
+
+  }
+
+  public getSearch(poke: string) {
+    const filter = this.allPokes.filter( (res: any) => {
+      return !res.name.indexOf(poke.toLowerCase());
+    })
+    this.getAllPokes = filter;
   }
 
 }
--- Projects/pokedex-project/src/app/shared/shared.module.ts
+++ Projects/pokedex-project/src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 // ----------- Components -----------
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
@@ -14,7 +15,8 @@ import { PokeListComponent } from './poke-list/poke-list.component';
   ],
   exports: [ PokeHeaderComponent, PokeSearchComponent, PokeListComponent ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ]
 })
 export class SharedModule { }
