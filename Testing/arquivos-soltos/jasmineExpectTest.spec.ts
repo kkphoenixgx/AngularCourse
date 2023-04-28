43c29e749770ac23ff391bef55c0dc1c1ea59924
Kauã Alves Santos <kauaAlvesWorkplace@gmail.com>
2023-04-28 14:52:54 -0300

    update to reboot pc

---
+++ Testing/arquivos-soltos/jasmineExpectTest.spec.ts
@@ -0,0 +1,7 @@
+/*
+  Question: how expect works
+  Conclusion: 
+*/
+
+//* expect() não é um método global ts, é um método global do Jasmine
+// Tudo só funciona no jasmine, siga para o jasmine-test
