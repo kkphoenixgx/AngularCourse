eb1f39c0efc0c13db0f15b9f6f3d6d20920014c3
Kauã Alves Santos <kauaalvesworkplace@gmail.com>
2023-08-24 23:45:24 -0300

    update to solve pc issues

---
+++ Projects/authFront Login-project/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCXFzO3_X8fOwJx8rKf-lGMbnMxf5Gfv-w",
+  authDomain: "authpage-c9de0.firebaseapp.com",
+  projectId: "authpage-c9de0",
+  storageBucket: "authpage-c9de0.appspot.com",
+  messagingSenderId: "292427340254",
+  appId: "1:292427340254:web:aaca2c2d6e8d5b9d0ae54f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
